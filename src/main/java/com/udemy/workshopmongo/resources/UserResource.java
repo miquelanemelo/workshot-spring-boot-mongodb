32c263d760857fbaa01f9f7364b2679d3fbeb383
Miquelane Melo <miquelanepmelo@gmail.com>
2022-08-30 18:20:47 -0300

    Entity user e REST

---
+++ src/main/java/com/udemy/workshopmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.udemy.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+	
+	@GetMapping(value = "/todos")
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "Maria", "maria@maria");
+		User alex = new User("2", "Alex", "alex@alex");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria,alex));
+		return ResponseEntity.ok().body(list);
+		
+	}
+
+}
