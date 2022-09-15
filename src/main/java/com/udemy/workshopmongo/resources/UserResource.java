170d1ecfb96e702c81f1b95c314cb3b75160e936
Miquelane Melo <miquelanepmelo@gmail.com>
2022-09-15 20:35:16 -0300

    usando padrao dto

--- src/main/java/com/udemy/workshopmongo/resources/UserResource.java
+++ src/main/java/com/udemy/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.udemy.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.dto.UserDto;
 import com.udemy.workshopmongo.service.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService userService;
 
 	@GetMapping(value = "/todos")
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDto>> findAll() {
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 
