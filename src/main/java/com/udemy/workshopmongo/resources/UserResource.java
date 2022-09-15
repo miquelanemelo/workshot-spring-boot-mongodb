dadfd0448e51ac356e04b4e1544820bf93852364
Miquelane Melo <miquelanepmelo@gmail.com>
2022-09-15 19:45:29 -0300

    conectando mongoDb ao repository e service

--- src/main/java/com/udemy/workshopmongo/resources/UserResource.java
+++ src/main/java/com/udemy/workshopmongo/resources/UserResource.java
@@ -1,28 +1,28 @@
 package com.udemy.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.service.UserService;
 
 @RestController
-@RequestMapping(value ="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
+	@Autowired
+	private UserService userService;
+
 	@GetMapping(value = "/todos")
-	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria", "maria@maria");
-		User alex = new User("2", "Alex", "alex@alex");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
-		
+
 	}
 
 }
---
+++ src/main/java/com/udemy/workshopmongo/service/UserService.java
@@ -0,0 +1,23 @@
+package com.udemy.workshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+		
+	}
+
+}
