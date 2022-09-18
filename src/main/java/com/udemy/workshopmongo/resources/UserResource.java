78da7d5e215adeda3a49ab11e0eeaeb4b16a3a6a
Miquelane Melo <miquelanepmelo@gmail.com>
2022-09-18 18:06:30 -0300

    adicionando exception global

--- src/main/java/com/udemy/workshopmongo/resources/UserResource.java
+++ src/main/java/com/udemy/workshopmongo/resources/UserResource.java
@@ -6,12 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.workshopmongo.domain.User;
 import com.udemy.workshopmongo.dto.UserDto;
-import com.udemy.workshopmongo.service.UserService;
+import com.udemy.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -25,6 +26,13 @@ public class UserResource {
 		List<User> list = userService.findAll();
 		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDto(obj));
 
 	}
 
--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -1,23 +1,29 @@
-package com.udemy.workshopmongo.service;
+package com.udemy.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.workshopmongo.domain.User;
 import com.udemy.workshopmongo.repository.UserRepository;
+import com.udemy.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
-		
+
+	}
+
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
