3bd5800fe9c70abcea1e649ea0905d40223be51d
Miquelane Melo <miquelanepmelo@gmail.com>
2022-09-18 19:01:32 -0300

    atualizazao com put

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -36,8 +36,22 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDto dto) {
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
+	
+	
 
 }
